ee269f59b1f66f35512f36a53ee0a06c4be7c9b8
Izan <izanfergi@gmail.com>
2024-06-01 18:22:17 +0200

    Llibre ya está fet

--- Llibres.java
+++ Llibres.java
@@ -7,38 +7,42 @@ public class Llibres{
     String nom;
     String autor;
     String genere;
-
-    public Llibres(){
-
+//posem les variables
+    public Llibres(int id, String nom, String autor, String genere){
+        this.id = id;
+        this.nom = nom;
+        this.autor = autor;
+        this.genere = genere;
     }
-    public void setId(){
-
+    public void setId(int id){
+        this.id = id;
     }
     public String getId() {
-    
+        return id;
     }
 
-    public void setNom(){
-
+    public void setNom(String nom){
+        this.nom = nom; 
     }
 
     public String getNom() {
-
+        return nom;
     }
 
-    public void setAutor(){
-
+    public void setAutor(String autor){
+        this.autor = autor;
     }
 
     public String getAutor() {
-
+        return autor;
     }
 
-    public void setGenere(){
-
+    public void setGenere(String genere){
+        this.genere = genere;
     }
 
     public int getGenere() {
-
+        return genere;
     }
 }
+//les truquem totes.
