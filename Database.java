af38666429aee99601c4e961f96f992066a8b5a1
Izan <izanfergi@gmail.com>
2024-06-01 19:20:52 +0200

    Que locura, voy a dejar esto ya por hoy porq llevo ya 3 examenes y esto ya me está matando x dentro

--- Database.java
+++ Database.java
@@ -33,9 +33,25 @@ public class Database{
         }
     }
     //Aqui el que fem es que hi volem crear un metode de desconexió amb la base de dades
-    public void addLlibre(){
-
+    public void addLlibre(Llibres llibres) throws SQLException{
+        String query = "INSERT INTO llibres (nom, autor, genere) VALUES (?, ?, ?)";
+        //Els ?? es per marcar les poscions dels valors
+        try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+        //Aquí el que hi fem sería crear un try per recuperar qualsevol clau generada com el ID del llibre.
+            statement.setString(1, llibres.getTitle());
+            statement.setString(2, llibres.getAuthor());
+            statement.setString(3, llibres.getGenre());
+            statement.executeUpdate();
+        // Aquí el que hi fem sería assignar els valors dels parametres
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    llibres.setId(generatedKeys.getInt(1));
+                }
+            }
+        //Aqui el que hi fem es que si s'ha generat un nou ID es ficara en el objecte llibres
+        }
     }
+    //Aquí el que hem fer una clase per insertar nous llibres de les bases de dades.
 
 }
 //Aqui ja hem fet totes les clases que necesitem per a la database.
