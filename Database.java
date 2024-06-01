61295cc046a820b31f5042f6c081cebb7a27d706
Izan <izanfergi@gmail.com>
2024-06-01 18:50:09 +0200

    Sinceramente no se que coño hacer, pero bueno a ver como lo hago.

--- Database.java
+++ Database.java
@@ -8,3 +8,33 @@ import java.util.List;
 
 //Importem totes les cosas que hi necessitem.
 
+public class Database{
+
+    private String llibURL;
+    private String llibUsername;
+    private String llibPassword;
+    private Connection connexio;
+
+    public Database(String llibURL,String llibUsername, String llibPassword ){
+        this.llibURL = llibURL;
+        this.llibUsername = llibUsername;
+        this.llibPassword = llibPassword;
+    }
+
+    public void connectar(){
+
+    }
+
+    public void desconnectar(){
+
+    }
+
+    public void addLlibre(){
+
+    }
+
+    public void getLlibre(){
+
+    }
+
+}
