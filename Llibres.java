97bdd80714c7e23f847d9ad8253e18a83cb05740
Izan <izanfergi@gmail.com>
2024-06-01 18:15:23 +0200

    Clases fetas en llibres, nomes toca emplenar i ja

--- Llibres.java
+++ Llibres.java
@@ -1,3 +1,44 @@
 //Comenzamos haciendo los libros, que será la parte mas senzilla en si.
 
 package examen;
+
+public class Llibres{
+    int id;
+    String nom;
+    String autor;
+    int quantiat;
+
+    public Llibres(){
+
+    }
+    public void setId(){
+
+    }
+    public String getIsbc() {
+    
+    }
+
+    public void setNom(){
+
+    }
+
+    public String getNom() {
+
+    }
+
+    public void setAutor(){
+
+    }
+
+    public String getAutor() {
+
+    }
+
+    public void setQuantiat(){
+
+    }
+
+    public int getQuantiat() {
+
+    }
+}
