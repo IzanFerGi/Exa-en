c5a60c3894074b10d404adf041f5e1b3f55ca77d
Izan <izanfergi@gmail.com>
2024-06-02 15:54:55 +0200

    No se que se debe de poner aquí aún , asi que seguiré avanzando un poco mas y luego lo relleno

--- Main.java
+++ Main.java
@@ -4,3 +4,9 @@ import com.example.database.models.Book;
 import java.sql.SQLException;
 
 
+public class Main{
+    public static void main(String[] args) {
+        String jdbcURL = "";
+        String jdbcUsername = "";
+        String jdbcPassword = "";
+}
